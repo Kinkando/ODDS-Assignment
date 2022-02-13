2ca66e14215c078e7f19b92d7370631ca46ceeb9
Tanawat Yuwansiri <TY_Sage@hotmail.com>
2022-02-13 22:21:12 +0700

    Add: Debug function for validate input

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// 12 => Odd1Even2
+// Add letter (Odd or Even) depend on type of integer (EX: 12 => Odd1Even2)
 function A(num) {
-    // Check only positive integer type and must not integer out of bound
-    if (parseInt(num) > Number.MAX_SAFE_INTEGER || parseInt(num) < 0) {
+    // Validate input format before execute
+    if (!verify('A', num)) {
         return "invalid input";
     }
     let numText = num.toString(); // Cast to String in order to iteration with charAt
@@ -13,8 +13,13 @@ function A(num) {
     return text;
 }
 
-// Odd1Even2 => DDO1NEVE2
+// Reverse letter and change alphabet to upper case (EX: Odd1Even2 => DDO1NEVE2)
 function B(text) {
+    // Validate input format before execute
+    if (!verify('B', text)) {
+        return "invalid input";
+    }
+
     let prev = 0; // Keep previous position (start index of text)
     let reverseText = ""; // Keep result that reverse text from parameter and change to upper case
 
@@ -30,8 +35,13 @@ function B(text) {
     // return text.replaceAll("Odd", "DDO").replaceAll("Even", "NEVE");
 }
 
-// DDO1NEVE2 => 6868791786986692
+// Convert Character to ASCII Code (EX: DDO1NEVE2 => 6868791786986692)
 function C(reverseText) {
+    // Validate input format before execute
+    if (!verify('C', reverseText)) {
+        return "invalid input";
+    }
+
     let ascii = ""; // Keep result of convert each character to ASCII Code
     for (let i = 0; i < reverseText.length; i++) {
         if (isNumber(reverseText.charAt(i))) {
@@ -44,8 +54,13 @@ function C(reverseText) {
     // return reverseText.replaceAll("DDO", "686879").replaceAll("NEVE", "78698669");
 }
 
-// 6868791786986692 => DDO1NEVE2
+// Convert ASCII code to Character (EX: 6868791786986692 => DDO1NEVE2)
 function D(ascii) {
+    // Validate input format before execute
+    if (!verify('D', ascii)) {
+        return "invalid input";
+    }
+
     // ascii = ascii.toString();
     let reverseText = ""; // Keep result of convert ASCII Code to reverse text 
     const numType = ["DDO", "NEVE"]; // Array for check converted text
@@ -61,8 +76,13 @@ function D(ascii) {
     // return ascii.replaceAll("686879", "DDO").replaceAll("78698669", "NEVE");
 }
 
-// DDO1NEVE2 => Odd1Even2
+// Reverse letter and change alphabet to Capital letter (EX: DDO1NEVE2 => Odd1Even2)
 function E(reverseText) {
+    // Validate input format before execute
+    if (!verify('E', reverseText)) {
+        return "invalid input";
+    }
+
     let prev = 0;
     let text = ""; // Keep result of convert reverse text to traditional text
     for (let i = 0; i < reverseText.length; i++) {
@@ -70,7 +90,7 @@ function E(reverseText) {
         if (isNumber(reverseText.charAt(i))) {
             // reverse all letters and change all letters to lower case, then concat with digit
             let word = reverseOrder(reverseText.slice(prev, i)).toLowerCase() + reverseText.charAt(i);
-            text += word.charAt(0).toUpperCase() + word.slice(1); // Change first alphabet to Capitalize letter
+            text += word.charAt(0).toUpperCase() + word.slice(1); // Change first alphabet to Capital letter
             prev = i + 1; // Shift next start alphabet index
         }
     }
@@ -78,8 +98,13 @@ function E(reverseText) {
     // return reverseText.replaceAll("DDO", "Odd").replaceAll("NEVE", "Even");
 }
 
-// Odd1Even2 => 12
+// Cut String Odd and Even (EX: Odd1Even2 => 12)
 function F(text) {
+    // Validate input format before execute
+    if (!verify('F', text)) {
+        return "invalid input";
+    }
+
     let numText = ""; // Cut all alphabets and keep only digits
     for (let i = 0; i < text.length; i++) {
         // Focus only integer and ignore all alphabets
@@ -186,6 +211,64 @@ function execute(number) {
 function removeChildren(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
+    }
+}
+
+// Check number format must be unsigned int and value must not out of bounds
+function isUnsignedInteger(text) {
+    return !(isNaN(text) || text.includes('.') || text.includes('-') || parseInt(text) > Number.MAX_SAFE_INTEGER);
+}
+
+// Verify input of each function (A, B, C, D, E, F)
+function verify(name, text) {
+    switch (name) {
+        case 'A':
+            return isUnsignedInteger(text); // Accepted only unsigned int type
+
+        case 'B':
+        case 'C':
+        case 'D':
+        case 'E':
+        case 'F':
+            // Create constant: type of number in list separate by order of function
+            const typeConst = name == 'D' ? ['686879', '78698669'] :
+                name == 'B' || name == 'F' ? ['Odd', 'Even'] : ['DDO', 'NEVE'];
+            let typeList = []; // Keep only type (Odd/Even) or (DDO/NEVE) depend on typeConst
+
+            // text remains have (Odd/Even) or (DDO/NEVE) letter
+            while (text.includes(typeConst[0]) || text.includes(typeConst[1])) {
+                let oddIndex = text.indexOf(typeConst[0]); // firstIndex of Odd or DDO
+                let evenIndex = text.indexOf(typeConst[1]); // firstIndex of Even or NEVE
+
+                // See Odd or DDO first || only have Odd or DDO
+                if ((oddIndex < evenIndex && oddIndex > -1) || (oddIndex != -1 && evenIndex == -1)) {
+                    typeList.push(text.slice(oddIndex, oddIndex + typeConst[0].length)); // Push Odd or DDO
+                    text = text.slice(0, oddIndex) + text.slice(oddIndex + typeConst[0].length); // Cut string Odd or DDO
+                }
+
+                // See Even or NEVE first || only have Even or NEVE
+                else if ((evenIndex < oddIndex && evenIndex > -1) || (evenIndex != -1 && oddIndex == -1)) {
+                    typeList.push(text.slice(evenIndex, evenIndex + typeConst[0].length)); // Push Even or NEVE
+                    text = text.slice(0, evenIndex) + text.slice(evenIndex + typeConst[1].length); // Cut string Even or NEVE
+                }
+            }
+
+            // length of typeList and leftText must be equals and leftText is in unsigned int type
+            let check = typeList.length == text.length && isUnsignedInteger(text);
+
+            // Iteration for each item in typeList compare to each position of leftText are the same type
+            for (let i = 0; i < typeList.length && check; i++) {
+                let item = typeList[i];
+                if (item == typeConst[0]) {
+                    check = parseInt(text.charAt(i).toString()) % 2 != 0; // Check current position of text is Odd number
+                } else {
+                    check = parseInt(text.charAt(i).toString()) % 2 == 0; // Check current position of text is Even number
+                }
+            }
+            return check;
+
+        default:
+            return false;
     }
 }
 
